db57cb88b9bdc980158d4986d187549e77df6ce1
niravtrivedi21 <niravtrivedi2112@gmail.com>
2018-10-29 16:21:46 +0530

    create remote method

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Member) {
+module.exports = function (Member) {
+
+    Member.remoteMethod('sayMyName', {
+        accepts: { arg: 'myName', type: 'string',description:'chuck in a name', required: true },
+        description: 'this one takes in a name and then says it',
+        http: { path: '/say-my-name/:myName', verb: 'get' },
+        returns: { arg: 'theResponse', type: 'object' }
+    });
+
+    Member.sayMyName = function (myname, callback) {
+        var theResponse = 'Your name is ' + myname;
+        callback(null,theResponse);
+    };
 
 };
